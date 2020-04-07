76f68a869104a06588aa7cd2e7b1db17414b15c5
Edwin <busine@live.com>
2020-04-07 17:45:45 -0400

    adding api key and env

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ const diagnosisSym = require("./model");
 const app = express();
 require("dotenv").config();
 
+const apiKey = process.env.APIKEY;
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -21,14 +23,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send(
-    "<h1>Covid-19 back-end</h1><p>Copyright © 2010 by Edwin Valerio & Carlos Ramirez</p>"
+    `<div>
+    <h1>Covid-19 back-end</h1><p>Copyright © 2010 by Edwin Valerio </p>
+    <p>You need an API key to use this service. Please contact me for a free API key at <a href="xvestudiox@gmail.com"> Edwin Valerio</a></p>
+    </div>`
   );
 });
 
-app.post("/diagnosis", async (req, res) => {
-  console.log(req.body);
-  const results = await diagnosisSym(req.body);
-  res.send(await results);
+app.post("/diagnosis/:apikey", async (req, res) => {
+  const userKey = req.params.apikey;
+  if (userKey == apiKey) {
+    const results = await diagnosisSym(req.body);
+    res.send(await results);
+  } else {
+    res.json({
+      code: 404,
+      message: "invalid API Key",
+    });
+  }
 });
 
 app.listen(port, () => {
